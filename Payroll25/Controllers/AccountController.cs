fa55c1081a52a4712431cdf544e38b00e6ea2793
Stefanus Terang <79499530+TerangStefanus@users.noreply.github.com>
2023-06-16 11:53:09 +0700

    Tes Layout

--- Payroll25/Controllers/AccountController.cs
+++ Payroll25/Controllers/AccountController.cs
@@ -12,37 +12,76 @@ namespace Payroll25.Controllers
     public class AccountController : Controller
     {
 
-        AccountDAO accDAO;
+        AccountDAO DAO;
 
         public AccountController()
         {
-            accDAO = new AccountDAO();
+            DAO = new AccountDAO();
         }
 
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult LoginKaryawan(LoginModel obj)
+        public IActionResult LoginAct(string username, string password)
         {
-            ClaimIdentity identity;
-            var dataKary = accDAO.GetAdmin(obj.USERNAME);
-            if(dataKary != null)
+            ClaimsIdentity identity = new ClaimsIdentity();
+            bool isAuth = false;
+            var data = DAO.GetKaryawan(username);
+
+            if(data != null)
             {
-                var hashpass = _encPassMhs(obj.PASSWORD);
-                if(hashpass == dataKary.PASSWORD)
+                //Data dicek masuk
+                var hashpass = _encPassMhs(password);// password di enkripsi menggunakan RIPEMD160
+                if(hashpass == data.password) 
                 {
+                    //Pengecekan Password aman
+                    isAuth = true;
                     identity = new ClaimsIdentity(new[] {
-                        new Claim(ClaimTypes.NameIdentifier , dataKary.USERNAME),
-                        new Claim(ClaimTypes.Name, dataKary.NAMA),
-                        new Claim(ClaimTypes.Role, dataKary.ROLE),
-                        new Claim("user", dataKary.NPP),
+                               new Claim(ClaimTypes.Name, data.nama),
+                               new Claim(ClaimTypes.Name, data.deskripsi),
+                               new Claim(ClaimTypes.Role, data.npp),
+                               new Claim("username", data.npp),
+                               new Claim("role", data.deskripsi),
+                               new Claim("menu", GenerateMenu(data.deskripsi))
                     }, CookieAuthenticationDefaults.AuthenticationScheme);
 
                     HttpContext.SignInAsync(
                        CookieAuthenticationDefaults.AuthenticationScheme,
                        new ClaimsPrincipal(identity));
                 }
-                
+                else
+                {
+                    // Password salah
+                    TempData["error"] = "Password Salah!";
+
+                }
             }
+            else
+            {
+                TempData["error"] = "Data Karyawan tidak ditemukan";
+            }
+
+            //return RedirectToAction("Index", "Home");
+
+            if (isAuth && data.deskripsi == "KSDM")
+            {
+                var principal = new ClaimsPrincipal(identity);
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            else if (isAuth)
+            {
+                var principal = new ClaimsPrincipal(identity);
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                return RedirectToAction("SlipGaji", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
         }
 
         private string _encPassMhs(string password)
@@ -57,7 +96,35 @@ namespace Payroll25.Controllers
             return hash;
         }
 
-        public IActionResult Index()
+        protected string GenerateMenu(string npp)
+        {
+            string menu = "";
+            List<ModelMenu> menus = new List<ModelMenu>();
+            List<ModelSubMenu> submenus = new List<ModelSubMenu>();
+
+            menus = DAO.GetMenuKaryawan(npp);
+            submenus = DAO.GetSubMenuKaryawan(npp);
+
+            if (menu != null)
+            {
+                foreach (var row in menus)
+                {
+                    menu += $"<li class='nav-item'><a href = '#' class='nav-link'><i class='nav-icon fas fa-circle'></i><p>{row.DESKRIPSI}<i class='fas fa-angle-left right'></i></p></a><ul class='nav nav-treeview'>";
+                    var filtersub = submenus.Where(x => x.ID_SI_MENU == row.ID_SI_MENU).ToList();
+
+                    foreach (var submenu in filtersub)
+                    {
+                        menu += $"<li class='nav-item'><a href='{submenu.LINK}' class='nav-link'><i class='far fa-circle nav-icon'></i><p>{submenu.DESKRIPSI}</p></a></li>";
+                    }
+
+                    menu += "</ul></li> ";
+                }
+            }
+
+            return menu;
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
