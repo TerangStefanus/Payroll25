9fb5bcff97d321d0a4f3ef01cc91050a86087ec0
Stefanus Terang <79499530+TerangStefanus@users.noreply.github.com>
2024-01-09 10:33:47 +0700

    feat: delete beberapa line code untuk privasi
    - Akses db UAJY dihapuskan

--- Payroll25/DAO/DBkoneksi.cs
+++ Payroll25/DAO/DBkoneksi.cs
@@ -3,12 +3,11 @@
     public class DBkoneksi
     {
         //internet publik
-        private static string IPdb = "202.14.92.208";
+        //private static string IPdb = "202.14.92.208";
 
         //internet lokal
         //private static string IPdb = "192.168.15.156";
 
-        public static string payrollkoneksi = $"Server={IPdb};Database=PAYROLL;User Id=payroll25;Password=payroll25123!";
-        public static string siatmakoneksi = $"Server={IPdb};Database=SIATMA_UAJY;User Id=payroll25;Password=payroll25123!";
+        //Ada bagian yang dihilangkan untuk privasi
     }
 }
