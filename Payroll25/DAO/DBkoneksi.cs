d0b3fcb0f4dafab40b6c35bc29a1b7554e2723a9
Stefanus Terang <79499530+TerangStefanus@users.noreply.github.com>
2024-05-27 16:59:31 +0700

    Last Commit

--- Payroll25/DAO/DBkoneksi.cs
+++ Payroll25/DAO/DBkoneksi.cs
@@ -3,11 +3,12 @@
     public class DBkoneksi
     {
         //internet publik
-        //private static string IPdb = "202.14.92.208";
+        private static string IPdb = "202.14.92.208";
 
         //internet lokal
         //private static string IPdb = "192.168.15.156";
 
-        //Ada bagian yang dihilangkan untuk privasi
+        public static string payrollkoneksi = $"Server={IPdb};Database=PAYROLL;User Id=payroll25;Password=payroll25123!";
+        public static string siatmakoneksi = $"Server={IPdb};Database=SIATMA_UAJY;User Id=payroll25;Password=payroll25123!";
     }
 }
